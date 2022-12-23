971493b477a522e949cd2b45d59d1481be30fadf
ilhammfadilah <ilhamfadilah92@gmail.com>
2022-12-23 21:31:51 +0700

    update command

--- bin/command.js
+++ bin/command.js
@@ -5,7 +5,8 @@ import { hideBin } from 'yargs/helpers';
 // import data from '../data.json' assert {type: "json"};
 import fs from 'fs';
 
-const data = fs.readFileSync('./data.json');
+const dataBuff = fs.readFileSync('./data.json');
+const data = JSON.parse(dataBuff);
 
 export function command() {
   yargs(hideBin(process.argv))
@@ -52,51 +53,22 @@ export function command() {
     argv => {
       const arg = argv['nomorsurah:ayat'];
       const arrArg = arg.split(':', 2);
+      if (arrArg[0] > 114) {
+        console.log('tidak ditemukan');
+        return;
+      }
       // console.log(arrArg);
       // const { nomorsurah, ayat } = argv;
       const rawSurah = fs.readFileSync('./data/'+arrArg[0]+'.json');
       const jsonSurah = JSON.parse(rawSurah)
       console.log('Terjemah dari '+jsonSurah[arrArg[0]]["name_latin"]+' ayat ke '+arrArg[1]);
       const result = jsonSurah[arrArg[0]]["translations"]["id"]["text"][arrArg[1]];
+      if (!result) {
+        console.log('mohon maaf data tidak ditemukan');
+        return;
+      }
       console.log(boxen(chalk.green(result), {padding: 1}));
     })
-  // .usage('Usage: $0 -a <arg1> -b <arg2>')
-  // .option('a', {
-  //   alias: 'arg1',
-  //   describe: 'Argument 1',
-  //   type: 'string',
-  // })
-  // .option('b', {
-  //   alias: 'arg2',
-  //   describe: 'Argument 2',
-  //   type: 'string',
-  // })
+    .demandCommand(1)
   .argv
 }
-
-// export function daftarSurah() {
-// yargs(hideBin(process.argv))
-//   .command('daftar', 'meanmpilkan daftar surah', () => {}, (argv) => {
-//     console.log(chalk.bgGreen.black('=== daftar surah ==='));
-//     data.forEach((surah, i) => {
-//       console.log(chalk.bold(`${i+1}\t: ${chalk.green(surah.nama)}`));
-//     })
-//   })
-//   .demandCommand(1)
-//   .help()
-//   .parse()
-// }
-
-// export function infoSurah() {
-//   yargs(hideBin(process.argv))
-//   .command('info <namasurah>', 'mencari detail surah', (yargs, helpOrVersionSet) => {
-//     return yargs.option('url', {
-//       alias: 'u',
-//       default: 'https://yargs.js.org'
-//     })
-//   }, (argv) => {
-//   })
-//   .demandCommand(1)
-//   .help()
-//   .parse()
-// }
